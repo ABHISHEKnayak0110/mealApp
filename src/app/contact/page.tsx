a324277fb853f43f4b0dad347eb3c35d211395f8
AbhishekNayak01 <abhishek.nayak@saarthi.ai>
2023-07-30 23:18:00 +0530

    hg

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,8 +6,8 @@ function page() {
         <h1>
             Name : Abhishek Nayak
         </h1>
-        <h1> Contact : 9754730724 </h1>
-        <h1> E mail : abhisheknayak0110@gmail.com </h1>
+        <h1> Contact : 9754730724</h1>
+        <h1> E mail : abhisheknayak0110@gmail.com</h1>
     </div>
   )
 }
